010e825249efd51ef97d28c845fbbc0f24908a88
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-06-06 12:16:47 +0200

    current state works

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,82 +6,74 @@ import shortid from 'shortid';
 import clsx from 'clsx';
 
 const Product = (props) => {
-  const [currentProduct, setCurrentSize, setCurrentColor] = useState({
-    currentColor: props.data.colors[0],
-    currentSize: props.data.sizes[0].name,
-    currentChoise: 'false',
-  });
+  const [currentColor, setCurrentColor] = useState(props.data.colors[0]);
+  const [currentSize, setCurrentSize] = useState(props.data.sizes[0].name);
+  console.log('props: ', props)
 
   const handleColor = (color) => {
-    
-    console.log('co dostaje handlecolor?: ',color);
-    setCurrentColor(currentProduct.currentColor === color)
-    console.log('handle color działa!: ', currentProduct)
+    setCurrentColor(color)
   };
 
-    const handleSize = (size) => {
-      
-      console.log('co dostaje handleSize? ', size);
-      setCurrentSize(currentProduct.currentSize === size);
-      console.log('handle size działa!: ', currentProduct)
-    };
+  const handleSize = (size) => {
+    setCurrentSize(size);
+  };
 
-    console.log('this is currentPoduct: ', currentProduct);
-    console.log('this is currentColor: ', currentProduct.currentColor);
+  console.log('this is currentSize: ', currentSize);
+  console.log('this is currentColor: ', currentColor);
 
-    const getColorClassName = (colorName) => {
-      switch (colorName) {
-        case 'blue':
-          return styles.colorBlue;
-        case 'red':
-          return styles.colorRed;
-        case 'green':
-          return styles.colorGreen;
-        case 'white':
-          return styles.colorWhite;
-        case 'black':
-          return styles.colorBlack;
-        //no default
-      }
+  const getColorClassName = (colorName) => {
+    switch (colorName) {
+      case 'blue':
+        return styles.colorBlue;
+      case 'red':
+        return styles.colorRed;
+      case 'green':
+        return styles.colorGreen;
+      case 'white':
+        return styles.colorWhite;
+      case 'black':
+        return styles.colorBlack;
+      //no default
     }
-
-    return (
-      <article className={styles.product}>
-        <div className={styles.imageContainer}>
-          <img
-            className={styles.image}
-            alt={props.data.title}
-            src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.data.name}--${currentProduct.currentColor}.jpg`} />
-        </div>
-        <div>
-          <header>
-            <h2 className={styles.name}>{props.data.title}</h2>
-            <span className={styles.price}>Price{props.data.basePrice + '$'}</span>
-          </header>
-          <form>
-            <div className={styles.sizes}>
-              <h3 className={styles.optionLabel}>Sizes</h3>
-              <ul className={styles.choices}>
-                {props.data.sizes.map(size => <Button
-                  className={currentProduct.currentSize === size.name && styles.active}
-                  action={handleSize} key={shortid()}>{size.name}</Button>)}
-              </ul>
-            </div>
-            <div className={styles.colors}>
-              <h3 className={styles.optionLabel}>Colors</h3>
-              <ul className={styles.choices}>
-                {props.data.colors.map(color => <Button className={clsx(getColorClassName(color), color === currentProduct.currentColor && styles.active)}
-                  action={handleColor} key={shortid()}>{color}</Button>)}
-              </ul>
-            </div>
-            <Button className={styles.button}>
-              <span className="fa fa-shopping-cart" />
-            </Button>
-          </form>
-        </div>
-      </article>
-    )
   }
 
-  //Product.propTypes = { props: PropTypes.any };
-  export default Product;
+  return (
+    <article className={styles.product}>
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          alt={props.data.title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.data.name}--${currentColor}.jpg`} />
+      </div>
+      <div>
+        <header>
+          <h2 className={styles.name}>{props.data.title}</h2>
+          <span className={styles.price}>Price{props.data.basePrice + '$'}</span>
+        </header>
+        <form>
+          <div className={styles.sizes}>
+            <h3 className={styles.optionLabel}>Sizes</h3>
+            <ul className={styles.choices}>
+              {props.data.sizes.map(size => <Button
+                className={currentSize === size.name && styles.active}
+                action={handleSize} key={shortid()}>{size.name}</Button>)}
+            </ul>
+          </div>
+          <div className={styles.colors}>
+            <h3 className={styles.optionLabel}>Colors</h3>
+            <ul className={styles.choices}>
+              {props.data.colors.map(color => <Button className={clsx(getColorClassName(color), color === currentColor && styles.active)}
+                action={handleColor} key={shortid()}>{color}</Button>)}
+            </ul>
+          </div>
+          <Button className={styles.button}>
+            <span className="fa fa-shopping-cart" />
+          </Button>
+        </form>
+      </div>
+    </article>
+  )
+}
+
+//Product.propTypes = { props: PropTypes.any };
+export default Product;
